50f373a603d86b3b63c2f58874b564ec3719c2ca
paloorazeez <paloorazeez@gmail.com>
2023-09-23 17:14:25 +0530

    first commit

---
+++ src/test/java/com/azeez/paloor/SpringConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azeez.paloor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
